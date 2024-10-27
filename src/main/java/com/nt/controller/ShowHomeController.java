0f2f4bfd5c75b5570d0fffde97ba6213e55d17cf
tharuni-gelli <tharuni.gelli@colorado.edu>
2024-10-27 23:19:58 +0530

    Removed the jsp page

--- src/main/java/com/nt/controller/ShowHomeController.java
+++ src/main/java/com/nt/controller/ShowHomeController.java
@@ -1,9 +1,13 @@
 package com.nt.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class ShowHomeController {
 	
 	
@@ -13,10 +17,10 @@ public class ShowHomeController {
 		return "welcome";
 	}*/
 	
-	@RequestMapping("/")
-	public  String  showHomePage() {
-		//return LVN
-		return "welcome";
+	@GetMapping("/wish")
+	public  ResponseEntity<String>  showWishMessage(){
+		System.out.println("WishMessageOperationsController.showWishMessage()");
+		return new  ResponseEntity<String>("Good Morning", HttpStatus.OK);
 	}
 
 }
